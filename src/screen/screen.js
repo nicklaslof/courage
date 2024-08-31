4fb095111930848810809494e04a3e4680592b53
Nicklas Löf <nicklaslof76@gmail.com>
2024-08-31 16:40:32 +0200

    Save levelprogress to localStorage

--- src/screen/screen.js
+++ src/screen/screen.js
@@ -10,19 +10,17 @@ import Fire from "../entity/fire.js";
 import Alien from "../entity/alien.js";
 import Thirteen from "../entity/thirteen.js";
 
+const localStorageLevel = "snukey.courage.level";
 class Screen{
     constructor(game,width, height){
         this.width = width;
         this.height = height;
 
         this.levels = [];
-        this.currentLevelId = 3;
-
-        //this.levels.push(this.tryAndCreateLevel(game,256,256,0,"Testroom",0xffcccccc,0xff999999,[Courage],0.0000009,{r:0.2,g:0.2,b:0.5,a:1.0},true,8,20,2));
         this.levels.push(this.tryAndCreateLevel(game,256,256,1,"Fear of insects",0xffcccccc,0xff999999,[Bug],0.09,{r:0.2,g:0.2,b:0.5,a:1.0},true,8,20,6,true,true));
         this.levels.push(this.tryAndCreateLevel(game,256,256,2,"Fear of spiders",0xff333333,0xff559955, [Spider],0.08,{r:0.3,g:0.3,b:0.35,a:1.0},true,8,15,10,true,true));
         this.levels.push(this.tryAndCreateLevel(game,256,256,2,"Spider mom",0xff333333,0xff559955, [Spider],0.08,{r:0.3,g:0.3,b:0.35,a:1.0},true,13,15,3,false,false,true));
-        this.levels.push(this.tryAndCreateLevel(game,256,256,3,"Fear of being alone",0xff666666,0xff666666, [Pickup],0.05,{r:0.3,g:0.3,b:0.35,a:1.0},true,8,15,9,true,true));
+        this.levels.push(this.tryAndCreateLevel(game,256,256,3,"Fear of being alone",0xff666666,0xff666666, [Pickup],0.06,{r:0.3,g:0.3,b:0.35,a:1.0},true,8,15,9,true,true));
         this.levels.push(this.tryAndCreateLevel(game,256,256,4,"Fear of darkness and ghosts",0xff666666,0xff666666, [Ghost],0.2,{r:0.0,g:0.0,b:0.0,a:1.0},false,8,15,9,true,true));
         this.levels.push(this.tryAndCreateLevel(game,256,256,5,"Fear of clowns",0xffcc99ff,0xff9999ff,[Clown],0.08,{r:0.3,g:0.3,b:0.3,a:1.0},true,8,15,9,true,true));
         this.levels.push(this.tryAndCreateLevel(game,256,256,5,"The clown",0xffcc99ff,0xff9999ff, [Clown],0.08,{r:0.3,g:0.3,b:0.3,a:1.0},true,13,15,3,false,false,true));
@@ -30,7 +28,7 @@ class Screen{
         this.levels.push(this.tryAndCreateLevel(game,256,256,7,"Fear of fire",0xff0044cc,0xff0085ff,[Fire],0.09,{r:0.1,g:0.1,b:0.5,a:1.0},false,8,15,9,true,true));
         this.levels.push(this.tryAndCreateLevel(game,256,256,8,"Fear of heights, outer space and aliens",0xff00ff00,0x00000000,[Alien],0.04,{r:0.4,g:0.4,b:0.5,a:1.0},false,20,25,4,false,false));
         this.levels.push(this.tryAndCreateLevel(game,256,256,9,"Fear of number 13",0xff222222,0xff555555,[Thirteen],0.09,{r:0.4,g:0.4,b:0.5,a:1.0},true,8,16,8,false,true));
-        this.tryChangeLevel(game, null);
+        this.tryChangeLevel(game, null,localStorage.getItem(localStorageLevel));
         
     }
 
@@ -39,22 +37,27 @@ class Screen{
         while (newLevel == null){
             let level = new Level(game,width,height,chapter,name,wallColor,floorColor,mobSpawns,mobSpawnChance,globalDarkness,torches,minRoomSize,maxRoomSize,numberOfRooms,lava,battleRoom,bossLevel);
             if (level.allRoomsCreated) newLevel = level;
-            else console.log("FAILED TO CREATE LEVEL");
-            //console.log(newLevel);
         }
         return newLevel;
     }
 
-    tryChangeLevel(game,player){
+    tryChangeLevel(game,player,levelId){
+        if (levelId == null) levelId = 0;
         if (this.currentLevelId == this.levels.length-1 && player.health >= 100){
             game.switchToEndScreen();
+            localStorage.setItem(localStorageLevel,0);
             return;
         }
-        if (player == null) 
-            this.level = this.levels[++this.currentLevelId];
-        else{
-            if (player.health >= 100) this.level = this.levels[++this.currentLevelId];
-            this.level.player.bombs = player.bombs;
+        if (player == null){
+            this.level = this.levels[levelId];
+            this.currentLevelId = levelId;
+        }else{
+            if (player.health >= 100){
+                this.level = this.levels[++this.currentLevelId];
+                localStorage.setItem(localStorageLevel,this.currentLevelId);
+                this.level.player.bombs = player.bombs;
+            }
+
         }
         
         if (player == null || player.health >= 100) this.levelTransitionTime = 300;
