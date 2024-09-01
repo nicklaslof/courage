0e539501cf04047f7dc1f2bf61c7f68e49986590
Nicklas Löf <nicklaslof76@gmail.com>
2024-09-01 10:24:28 +0200

    Clown battle tweaks

--- src/entity/clown.js
+++ src/entity/clown.js
@@ -7,9 +7,11 @@ import Pickup from "./pickup.js";
 class Clown extends Enemy{
         
     constructor(x,y,c,speed,size,boss){
-        super(x,y,new Sprite(x,y,0,80,9,14,size,size,0xffffffff));
+        super(x,y,new Sprite(x,y,0,80,9,14,Math.max(28,size),Math.max(28,size),0xffffffff));
         this.speed = 20;
         this.c = 0xffffffff;
+        this.health = 2;
+        size = Math.max(28,size);
 
         this.collisionBox = {minX:0,minY:0,maxX:size+2,maxY:size+6};
         this.updateAABB();
@@ -109,11 +111,11 @@ class Clown extends Enemy{
                     this.calculatePlayerDirectionVector.y = this.throwLocation.y + game.getRandom(-16,16) - this.y;
                     if (this.throwCountdown <= 0){
                         this.throwCounter++;
-                        this.throwCountdown = 100;
+                        this.throwCountdown = 60;
                         if (game.length(this.calculatePlayerDirectionVector) < 180 && game.canEntitySee(game.screen.level,this.throwLocation.x,this.throwLocation.y,this.x,this.y)) {
                             this.animation.setCurrentState("attack");
                             this.normalize(this.calculatePlayerDirectionVector);
-                            game.screen.level.addEntity(new Bullet(this.x+18,this.y+16,6000,100, this.calculatePlayerDirectionVector.x, this.calculatePlayerDirectionVector.y,this,0xff0000ff));
+                            game.screen.level.addEntity(new Bullet(this.x+18,this.y+16,1500,280, this.calculatePlayerDirectionVector.x, this.calculatePlayerDirectionVector.y,this,0xff0000ff));
 
                         }
                     }
