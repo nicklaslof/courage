fd083f15aaf8363236a3a532a5c5d9c5765b0564
Nicklas Löf <nicklaslof76@gmail.com>
2024-09-02 09:29:03 +0200

    Bigger particles

--- src/entity/enemy.js
+++ src/entity/enemy.js
@@ -20,7 +20,7 @@ class Enemy extends Entity{
 
     onDeath(game){
         for (let i = 0; i < 32;i++){
-            game.screen.level.addParticle(this.x,this.y+this.pixelScale,0x990000ff,game.getRandom(1,12),game.getRandom(1,12),1500,{x:game.getRandom(-0.5,0.5),y:game.getRandom(-0.7,-0.3)},game.getRandom(50,120));
+            game.screen.level.addParticle(this.x,this.y+this.pixelScale,0x990000ff,game.getRandom(5,20),game.getRandom(5,20),1500,{x:game.getRandom(-0.5,0.5),y:game.getRandom(-0.7,-0.3)},game.getRandom(50,120));
         }
         game.screen.level.addLight(this.x,this.y,0xff00ffff,48,48,100,false);
         if (Math.random()< 0.6){
@@ -40,7 +40,7 @@ class Enemy extends Entity{
         if (this.health>0){
             game.playEnemyHit();
             for (let i = 0; i < 4;i++){
-                game.screen.level.addParticle(this.x,this.y+this.pixelScale,0x990000ff,game.getRandom(1,4),game.getRandom(1,4),600,{x:game.getRandom(-0.5,0.5),y:game.getRandom(-0.7,-0.3)},100);
+                game.screen.level.addParticle(this.x,this.y+this.pixelScale,0x990000ff,game.getRandom(5,8),game.getRandom(5,8),600,{x:game.getRandom(-0.5,0.5),y:game.getRandom(-0.7,-0.3)},100);
             }
         }
         if(this.boss) game.playBossHit();
